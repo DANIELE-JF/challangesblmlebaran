f68be102511feeb7ae48e2e7502d63075e4078be
DANIELE-JF <139364490+DANIELE-JF@users.noreply.github.com>
2024-04-06 22:44:47 +0700

    Add files via upload

--- challange13.py
+++ challange13.py
@@ -1,21 +1,49 @@
 import os
 
+def median(*args):
+    sorted_args = sorted(args)
+    n = len(sorted_args)
+    if n % 2 == 0:
+        return (sorted_args[n // 2 - 1] + sorted_args[n // 2]) / 2
+    else:
+        return sorted_args[n // 2]
+
+def modulus(*args):
+    counts = {}
+    for num in args:
+        if num in counts:
+            counts[num] += 1
+        else:
+            counts[num] = 1
+    max_count = max(counts.values())
+    modes = [num for num, count in counts.items() if count == max_count]
+    return modes[0]
+
 def main():
     numbers = []
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')  # Membersihkan layar
-        num_input = input("Masukkan angka: ")
-        if num_input.replace('.', '', 1).isdigit():
-            numbers.append(float(num_input))
+        os.system('cls' if os.name == 'nt' else 'clear')  
+        num_input = input("Masukkan angka : ")
+        if num_input.replace('.', '', 1).isdigit(): 
+            num = int(num_input)
+            numbers.append(num)
         else:
             print("Input tidak valid. Masukkan angka.")
+            continue
         
-        if input("Lanjut y/n: ").lower() != 'y':
+        lanjut = input("Lanjut y/n : ")
+        if lanjut.lower() != 'y':
             break
 
     if numbers:
-        print(f"Hasil mean dari data: {numbers} = {sum(numbers) / len(numbers):.2f}")
+        avg = sum(numbers) / len(numbers)
+        med = median(*numbers)
+        mod = modulus(*numbers)
+        print(f"Deret angka = {numbers}")
+        print(f"Hasil mean = {avg:.2f}")
+        print(f"Hasil median = {med}")
+        print(f"Hasil modus = [{mod}]")
     else:
-        print("No data .")
+        print("Tidak ada data untuk dihitung.")
 
 main()
